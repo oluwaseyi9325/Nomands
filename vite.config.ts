71f996280c749e0788b0f57faec15e66ff5e8097
oluwaseyi9325 <adedokunseyi96@gmail.com>
2025-04-16 15:24:38 +0100

    install tanstack query

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, PluginOption } from "vite"
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), TanStackRouterVite({ target: 'react', autoCodeSplitting: true }) as unknown as PluginOption],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -13,6 +14,7 @@ export default defineConfig({
   },
   server: {
     port:3000
-  }
+  },
+  
 
 })
